2f539dd49fec53bf526b285fb2f93e34538a859c
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-05 17:27:31 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/apify/actor/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Apify Actor",
+    description = "This sub-group of plugins contains tasks to interact with Apify Actors.",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.apify.actor;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
