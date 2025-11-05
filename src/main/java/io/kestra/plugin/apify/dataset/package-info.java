850323c9af15e9ce4d8f197606f1bc74c25b5481
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-05 17:27:31 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/apify/dataset/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Apify Dataset",
+    description = "This sub-group of plugins contains tasks to interact with Apify Datasets.",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.apify.dataset;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
